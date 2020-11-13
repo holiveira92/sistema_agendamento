c82a47f70bc8d273512672aee12211ac670aaa95
Hiago Oliveira <hiagojardimdeoliveira@hotmail.com>
2020-11-12 22:23:08 -0300

    iniciando arquit

--- public/js/app.js
+++ public/js/app.js
@@ -19384,8 +19384,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\sistema_gerenciamento\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\sistema_gerenciamento\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\sistema_agendamento\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\sistema_agendamento\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
